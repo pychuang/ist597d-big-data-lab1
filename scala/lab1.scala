6b7df6b77f912ef529e071a0fbc88de664b136b5
Po-Yu Chuang <ratbert.chuang@gmail.com>
2015-09-20 17:56:59 -0400

    scala/lab1.scala: read JSON file OK
    I give up. The JSON library for Scala is not as easy to use as Python.

--- scala/lab1.scala
+++ scala/lab1.scala
@@ -1,7 +1,7 @@
 import java.io.File
 import java.io.PrintWriter
 
-//import org.json4s._
+import org.json4s._
 //import org.json4s.jackson.JsonMethods._
 import org.json4s.jackson.JsonMethods
 
@@ -9,15 +9,30 @@ import scala.io.Source
 
 object Lab1
 {
-  def main(args: Array[String])
-  {
+  def handle_tweet(t:Map[String, String]): Unit = {
+    print("T: ")
+    println(t)
+    print("E: ")
+    println(t("enetities"))
+  }
+
+  def main(args: Array[String]) {
 //     val writer = new PrintWriter( new File("output.txt") )
 //     writer.write("Following is the content read:" )
 //     Source.fromFile("tweets-set1").foreach { x => writer.write(x) }
 //     writer.close()
-    val source = io.Source.fromFile(args(0))
-    val lines = try source.mkString finally source.close()
-    val parsed_json = JsonMethods.parse(lines)
+    println("args")
+    args.foreach(println)
+//    Source.fromFile(args(0)).getLines.foreach(x=>println("LINE: " + x))
+    val lines = Source.fromFile(args(0)).getLines
+
+    val tweets_json = lines.map(line=>line.split("[|]")).filter(_.size > 1).map(_(1))
+    val tweets = tweets_json.map(j=>JsonMethods.parse(j).asInstanceOf[JObject].values)
+//    val tweets = tweets_json.map(j=>JsonMethods.parse(j).values)
+//    tweets.foreach(handle_tweet)
+    tweets.foreach(t=>println("\nT: " + t))
+//    tweets.foreach(t=>JsonMethods.pretty(JsonMethods.render(t.asInstanceOf[JValue])))
+//    val parsed_json = JsonMethods.parse(lines)
 
   }
 
